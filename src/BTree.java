378af63ff27ea405b9317b381dce887376ec85cc
Ángel Guerrero <_angelguerrero_@outlook.com>
2018-03-27 13:11:28 -0600

    Completando la evidencia de aprendizaje.

--- src/BTree.java
+++ src/BTree.java
@@ -1,20 +1,15 @@
-import java.util.List;
-
 /**
  * Clase Binary Tree
  */
 public class BTree {
     private int size;
     private Node root;
-    private int maxLevel;
-    private MyList<Pair> wideList;
+    private Node minor;
 
     // Constructor
     public BTree() {
         this.root = null;
         this.size = 0;
-        this.maxLevel = 0;
-        wideList = new MyList<>();
     }
 
 
@@ -22,8 +17,6 @@ public class BTree {
     public BTree(Node root) {
         this.root = root;
         this.size = 0;
-        this.maxLevel = 0;
-        wideList = new MyList<>();
     }
 
 
@@ -37,77 +30,11 @@ public class BTree {
 
         if (root == null) {
             this.setRoot(el);
-            root.setLevel(maxLevel++);
-            System.out.println("Se ha establecido el valor " + data + " para el nodo raíz.\n");
+            size++;
+            System.out.println("\nSe ha establecido el valor " + data + " para el nodo raíz.\n");
         } else {
             add(data, root);
         }
-    }
-
-
-    /**
-     * Muestra los elementos del árbol de acuerdo a los niveles
-     * del árbol, es decir, partiendo de la fuente root como
-     * nivel 0, hasta el último nodo y su respectivo nivel de
-     * profundidad.
-     */
-    public void showWide() {
-
-        if (root == null) {
-            System.out.println("El árbol está vacío.");
-        } else {
-            // Verifica que haya datos en la lista wideList
-            fillWideList(root);
-
-            System.out.println("\nMostrando los datos en por anchura:");
-
-            List<Pair> li = wideList.getAll();
-            int len = getMaxLevel();
-
-            for (int i = 0; i <= len; i++) {
-                System.out.printf("Nivel " + i + ": \t");
-                for (Pair<Integer, Integer> p : li) {
-                    if (i == p.getFirst()) {
-                        System.out.printf(p.getSecond() + ", \t");
-                    }
-                }
-                System.out.println();
-            }
-        }
-    }
-
-
-    /**
-     * Verifica si el árbol está vacío
-     *
-     * @return boolean  Regresa verdadero si el árbol está vacío,
-     * si no, regresa falso.
-     */
-    public boolean isEmpty() {
-        return this.size == 0;
-    }
-
-
-    /**
-     * Obtiene el nivel máximo de profundidad del árbol binario.
-     * @return
-     */
-    public int getMaxLevel() {
-        return maxLevel;
-    }
-
-    //===================================================
-    //  Private methods
-    //===================================================
-
-
-    /**
-     * Establece al nodo raíz del árbol.
-     *
-     * @param root Nodo que será el nodo raíz del árbol.
-     */
-    private void setRoot(Node root) {
-        this.root = root;
     }
 
 
@@ -129,11 +56,13 @@ public class BTree {
                 // Agrega el nuevo nodo izquierdo
                 root.setLeft(new Node(data));
 
+                // Establece el padre del nuevo nodo
+                root.getLeft().setFather(root);
+
                 // Agrega su nivel de profundidad
                 root.getLeft().setLevel(root.getLevel() + 1);
 
-                // Establece el nivel máximo de profundidad del árbol
-                maxLevel = root.getLeft().getLevel() > maxLevel ? root.getLeft().getLevel() : maxLevel;
+                size++;
 
                 System.out.println("Agregando hijo: " + root.getLeft().getData() + " <- a padre: " + root.getData());
             } else {
@@ -145,11 +74,13 @@ public class BTree {
                 // Agrega el nuevo nodo derecho
                 root.setRight(new Node(data));
 
+                // Establece el padre del nuevo nodo
+                root.getRight().setFather(root);
+
                 // Agrega su nivel de profundidad
                 root.getRight().setLevel(root.getLevel() + 1);
 
-                // Establece el nivel máximo de profundidad del árbol
-                maxLevel = root.getRight().getLevel() > maxLevel ? root.getRight().getLevel() : maxLevel;
+                size++;
 
                 System.out.println("Agregando a padre: " + root.getData() + " -> un hijo: " + root.getRight().getData());
             } else {
@@ -161,24 +92,114 @@ public class BTree {
 
 
     /**
-     * Esta función es un método auxiliar para obtener los datos
-     * del árbol binario de acuerdo a los niveles de profundidad.
-     * Este método es un método inneficiente que verifica si el
-     * elemento que se va a agregar esté o no en la lista, ya que
-     * de acuerdo a la recursividad del método showWide() y fillWideList
-     * si no se verifica el dato en la lista, éste se repetirá tantas
-     * veces sea por la recursividad del método fileWideList.
-     *
-     * @param data Dato a buscar en la lista.
-     * @return boolean Regresa true si el elemento ya está en la lista,
-     * de lo contrario regresa false.
+     * Llama a la función privada preOrder.
      */
-    private boolean contains(Pair data) {
-        boolean retval = false;
+    public void preOrder() {
+        preOrder(root);
+    }
 
-        for (Pair<Integer, Integer> l : wideList.getAll()) {
-            if (l.getFirst() == data.getFirst() && l.getSecond() == data.getSecond()) {
-                retval = true;
+
+    /**
+     * Muestra los datos en forma de preOrder
+     * esta es una función adicional a la pública
+     * por la razón de que se está llamando recursivamente.
+     *
+     * @param root Nodo actual desde donde se verificarán
+     *             los datos de los nodos hijos.
+     */
+    private void preOrder(Node root) {
+        if (root != null) {
+            System.out.print(root.getData() + " - ");
+            preOrder(root.getLeft());
+            preOrder(root.getRight());
+        }
+    }
+
+
+    /**
+     * Llama a la función privada InOrder.
+     */
+    public void inOrder() {
+        inOrder(root);
+    }
+
+
+    /**
+     * Muestra los datos en forma de InOrder
+     * es una función privada ya que a diferencia
+     * de la función pública del mismo nombre,
+     * ésta se está llamando recursivamente.
+     *
+     * @param root Nodo actual desde donde se verificarán
+     *             los datos de los nodos hijos
+     */
+    private void inOrder(Node root) {
+        if (root != null) {
+            inOrder(root.getLeft());
+            System.out.print(root.getData() + " - ");
+            inOrder(root.getRight());
+        }
+    }
+
+
+    /**
+     * Llama a la función privada postOrder.
+     */
+    public void postOrder() {
+        postOrder(root);
+    }
+
+
+    /**
+     * Muestra los datos en la forma PostOrder
+     * esta función privada a diferencia de la
+     * función pública del mismo nombre,
+     * se está llamando de forma recursiva.
+     *
+     * @param root Nodo actual desde donde se verificarán
+     *             los datos de los nodos hijos.
+     */
+    private void postOrder(Node root) {
+        if (root != null) {
+            postOrder(root.getLeft());
+            postOrder(root.getRight());
+            System.out.print(root.getData() + " - ");
+        }
+    }
+
+
+    /**
+     * Obtiene el elemento menor del árbol.
+     * <p>
+     * Para la implementación de este método es necesario verificar
+     * que el árbol no esté vacío, si no lo está entonces manda el
+     * dato del nodo.
+     *
+     * @return int Si el árbol está vacío devuelve un número negativo
+     * el cual es: -999, se debe de tener cuidado al tratar
+     * con este error, ya que no es posible devolver un
+     * número de error como tal ya que se espera que devuelva
+     * un número entero, y el número entero podría estar dentro
+     * del árbol, CAUSANDO UN BUG EN EL PROGRAMA.
+     */
+    public int getMinor() {
+        int retval = -999;
+
+        if (isEmpty()) {
+            System.out.println("El árbol está vacío.");
+        } else {
+            if (size == 1) {
+                retval = root.getData();
+            } else {
+                minor = root;
+                Node aux = null;
+
+                while (minor != null) {
+                    retval = minor.getData();
+                    aux = minor;
+                    minor = minor.getLeft();
+                }
+                minor = aux;
             }
         }
 
@@ -187,47 +208,69 @@ public class BTree {
 
 
     /**
-     * Este método privado es utilizado para rellenar los datos
-     * del árbol binario en un lista donde se pueda observar
-     * la profundidad del elemento y su correspondiente valor.
-     *
-     * @param el Nodo del que partirá para verificar los hijos,
-     *           izquierdo y derecho.
+     * Eliminar el elemento menor del árbol binario.
      */
-    private void fillWideList(Node el) {
-        if (el != null) {
+    public void removeMinor() {
+        int numMin = getMinor();
 
-            // Si el elemento no está en la lista, lo agrega
-            if (!contains(new Pair(el.getLevel(), el.getData()))) {
-                wideList.add(new Pair(el.getLevel(), el.getData()));
-            }
+        if (numMin != -999) {
+            // El puntero menor ya está posicionado, no hace
+            // falta realizar todo el recorrido nuevamente,
+            // solo verifica que el elemento a eliminar sea
+            // el único elemento del árbol
+            if (size == 1) {
+                System.out.println("Eliminando el primer elemento: " + root.getData());
+                setRoot(null);
+                size--;
+            } else {
+                if (minor == root) {
+                    if (root.getLeft() != null) {
+                        setRoot(root.getLeft());
+                        root.setRight(minor.getRight());
+                        minor.getRight().setFather(root);
 
-            // Si el nodo izquierdo existe
-            if (el.getLeft() != null) {
-                el.getLeft().setFather(el);
-                el.getLeft().setLevel(el.getLevel() + 1);
-
-                // Si el elemento no está en la lista, lo agrega
-                if (!contains(new Pair(el.getLevel(), el.getData()))) {
-                    wideList.add(new Pair(el.getLevel(), el.getData()));
+                        minor.setLeft(null);
+                        minor.setRight(null);
+                    } else {
+                        setRoot(root.getRight());
+                        root.setFather(null);
+                        minor.setRight(null);
+                    }
+                } else {
+                    // Si este nodo tiene más nodos hijos a su derecha
+                    if (minor.getRight() != null) {
+                        Node childRight = minor.getRight();
+                        minor.getFather().setLeft(childRight);
+                    } else {
+                        minor.getFather().setLeft(null);
+                    }
                 }
-
-                fillWideList(el.getLeft());
-            }
-
-            // Si el nodo derecho existe
-            if (el.getRight() != null) {
-                el.getRight().setFather(el);
-                el.getRight().setLevel(el.getLevel() + 1);
-
-                // Si el elemento no está en la lista, lo agrega
-                if (!contains(new Pair(el.getLevel(), el.getData()))) {
-                    wideList.add(new Pair(el.getLevel(), el.getData()));
-                }
-
-                fillWideList(el.getRight());
+                minor = null;
+                size--;
+                System.out.println("Elemento eliminado correctamente.");
             }
         }
+    }
+
+
+    /**
+     * Verifica si el árbol está vacío
+     *
+     * @return boolean  Regresa verdadero si el árbol está vacío,
+     * si no, regresa falso.
+     */
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+
+    /**
+     * Establece al nodo raíz del árbol.
+     *
+     * @param root Nodo que será el nodo raíz del árbol.
+     */
+    private void setRoot(Node root) {
+        this.root = root;
     }
 
 
@@ -239,33 +282,13 @@ public class BTree {
         private Node father;
         private Node left;
         private Node right;
-        private boolean visited;
-        private boolean discovered;
         private int level;
 
         public Node(int data) {
             this.data = data;
             this.left = null;
             this.right = null;
-            this.visited = false;
-            this.discovered = false;
             this.level = -1;
-        }
-
-        public boolean isVisited() {
-            return visited;
-        }
-
-        public void setVisited(boolean visited) {
-            this.visited = visited;
-        }
-
-        public boolean isDiscovered() {
-            return discovered;
-        }
-
-        public void setDiscovered(boolean discovered) {
-            this.discovered = discovered;
         }
 
         public int getLevel() {
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,123 @@
-import java.util.List;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-public class Main {
+import java.net.URL;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+public class Main extends Application implements Initializable{
+
+    private static BTree btree;
+
+    @FXML private Button randomBtn;
+    @FXML private Button deleteBtn;
+    @FXML private Button showPreorderBtn;
+    @FXML private Button showInorderBtn;
+    @FXML private Button showPostorderBtn;
+    @FXML private Button exitBtn;
+
+    private Alert alert;
+
 
     public static void main(String[] args) {
-        BTree btree = new BTree();
+        btree = new BTree();
 
-        btree.add(2);
-        btree.add(7);
-        btree.add(6);
-        btree.add(5);
-        btree.add(11);
-        btree.add(9);
-        btree.add(4);
-        btree.add(3);
-        btree.add(1);
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader root = new FXMLLoader(getClass().getResource("Main.fxml"));
+
+        Scene scene = new Scene(root.load());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Métodos de árbol binario");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        // Alertas para la aplicación
+        alert = new Alert(Alert.AlertType.NONE);
+
+        // Pone a la escucha los botones de la ventana
+        randomBtn.setOnAction(e -> addRandom());
+
+        deleteBtn.setOnAction(e -> delMinor());
+
+        showPreorderBtn.setOnAction(e -> preorder());
+        showInorderBtn.setOnAction(e -> inorder());
+        showPostorderBtn.setOnAction(e -> postOrder());
+
+        exitBtn.setOnAction(e -> closeApp());
+    }
+
+
+    private void addRandom() {
+        Random random = new Random(System.currentTimeMillis());
+
+        int numrand = random.nextInt(100);
+
+        btree.add(numrand);
+
+        alert.setAlertType(Alert.AlertType.INFORMATION);
+        alert.setContentText("El elemento: " + numrand + " se ha agregado al árbol.");
+        alert.setTitle("Elemento agregado.");
+        alert.show();
+    }
+
+
+    /**
+     * Elimina el elemento menor del árbol binario.
+     */
+    private void delMinor() {
+        btree.removeMinor();
+    }
+
+
+    /**
+     * Muestra los datos en preorden
+     */
+    private void preorder() {
+        System.out.println("\nElementos en PreOrder: ");
+        btree.preOrder();
         System.out.println();
+    }
 
-        btree.showWide();
+
+    /**
+     * Muestra los datos en inorden.
+     */
+    private void inorder() {
+        System.out.println("\nElementos en InOrder: ");
+        btree.inOrder();
+        System.out.println();
+    }
+
+
+    /**
+     * Muestra los datos en postorden.
+     */
+    private void postOrder() {
+        System.out.println("\nElementos PostOrder: ");
+        btree.postOrder();
+        System.out.println();
+    }
+
+
+    /**
+     * Cierra la aplicación.
+     */
+    private void closeApp() {
+        Platform.exit();
     }
 }
